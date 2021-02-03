3335866a394ba032ca06f0ce85a5c47efdcba398
or <orbenabba@gmail.com>
2021-02-03 19:50:56 +0200

    Purchase is done

--- contracts/Purchase.sol
+++ contracts/Purchase.sol
@@ -3,7 +3,10 @@ import "./Product.sol";
 
 contract Purchase {
     address public buyer;
-    mapping(id => Product.Item) public products;
+    ///////////////////////////////
+    Product.Item[] public items;
+    address payable[] public addresses;
+    ///////////////////////////////
     uint256 tempBalance;
 
     constructor() {
@@ -17,26 +20,43 @@ contract Purchase {
         string memory _description,
         uint256 _publishedDate,
         uint256 _price,
-        address seller
+        address payable seller
     ) public returns (bool) {
         if (tempBalance < _price) {
             return false;
         }
 
-        products[seller] = Product.Item(
-            _name,
-            _description,
-            _publishedDate,
-            _price
-        );
+        items.push(Product.Item(_name, _description, _publishedDate, _price));
+        addresses.push(seller);
         tempBalance -= _price;
         return true;
     }
 
-    function removeProduct(address seller, uint256 price) public {
-        delete (products[seller]);
+    /*
+    function removeProduct(address seller, uint price)public{
+        remove()
         tempBalance += price;
+    }*/
+
+    //Transfer all the money to all the sellers
+    function commitShopping() public {
+        for (uint256 i = 0; i < items.length; i++) {
+            addresses[i].transfer(items[i].price);
+        }
     }
 
-    function commitShopping() public {}
+    /*
+    function remove(uint index) public returns(uint[]) {
+        if (index >= items.length) return;
+
+        for (uint i = index; i<items.length-1; i++){
+            items[i] = items[i+1];
+            addresses[i] = addresses[i+1];
+        }
+        delete items[items.length-1];
+        delete addresses[addresses.length-1];
+        items.length--;
+        addresses.length--;
+        return items;
+    }*/
 }
