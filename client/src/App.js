53229bfc3ebf8999a4c09b6f2433e843dc14517b
or <orbenabba@gmail.com>
2021-02-10 16:10:21 +0200

    First integration with deploying the contracts into the UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+
+import JokerzonContract from "./contracts/Jokerzon.json";
+import ProductContract from "./contracts/Product.json";
+import PurchaseContract from "./contracts/Purchase.json";
+
 import getWeb3 from "./getWeb3";
 import {BrowserRouter as Router,Route} from "react-router-dom";
 
@@ -26,15 +30,13 @@ class App extends Component {
 
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = JokerzonContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        JokerzonContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
-
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance }, this.runExample);
@@ -48,6 +50,7 @@ class App extends Component {
   };
 
   runExample = async () => {
+    /*
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
@@ -57,18 +60,19 @@ class App extends Component {
     const response = await contract.methods.get().call();
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ storageValue: response });*/
   };
 
   render() {
     if (!this.state.web3) {
       return <div>Please connect your Meta-Mask Wallet</div>;
     }
+    console.log("this.state.contract = ",this.state.contract);
     return (
       <div>
           <Router>
               <Navbar></Navbar>
-              <Route path="/" exact component={Jokerzon}></Route>
+              <Route path="/" render={props => <Jokerzon jokerzonContract={this.state.contract}/>}></Route>
               <Route path="/shopping" component={Shopping}></Route>
               <Route path="/selling" component={Checkout}></Route>
           </Router>
--- client/src/components/Jokerzon/Home.js
+++ client/src/components/Jokerzon/Home.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { homeObjOne, homeObjTwo, homeObjThree/*, homeObjFour*/ } from './Data';
 import InfoSection from './InfoSection';
 
-export default function Home() {
+export default function Home(props) {
   return (
     <>
+      <h1 style={{color:"white"}}>Jokerzon cotract address = {props.jokerzonContract._address}</h1>
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
       <InfoSection {...homeObjThree} />
